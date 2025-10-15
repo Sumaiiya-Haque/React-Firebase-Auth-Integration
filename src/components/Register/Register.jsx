f2f622536d02e16acd29c60601c4912ce1be8bc8
Sumaiiya-Haque <sumaiyahaque475@gmail.com>
2025-10-15 17:05:13 +0600

    authcontext

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,25 +1,30 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+// import { createUserWithEmailAndPassword } from "firebase/auth";
+import { use } from "react";
 import { Link } from "react-router";
-import { auth } from "../../firebase/firebase.init";
-
+import { AuthContext } from "../../context/AuthContext/AuthContext";
+// import { auth } from "../../firebase/firebase.init";
 
 const Register = () => {
-//   const userInfo = use(AuthContext);
-//   console.log('in registration', userInfo);
-
-  const handleRegister = (e) => {
-    e.preventDefault();
-    const email = e.target.email.value;
-    const password = e.target.password.value;
-    console.log(email, password);
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  const authInfo = use(AuthContext);
+  const handleRegister = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
   };
+
+  // const handleRegister = (e) => {
+  //   e.preventDefault();
+  //   const email = e.target.email.value;
+  //   const password = e.target.password.value;
+  //   console.log(email, password);
+  //   createUserWithEmailAndPassword(auth, email, password)
+  //     .then((result) => {
+  //       console.log(result);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // };
 
   return (
     <div className="card bg-base-100 m-auto w-full max-w-sm shrink-0 shadow-2xl">
